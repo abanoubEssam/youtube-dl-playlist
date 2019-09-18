4c05a8058800130e20dc47165267e3022843160d
Abanoub Essam <abanoub@index-soft.com>
2019-09-18 16:14:37 +0200

    on progress

--- app.js
+++ app.js
@@ -3,84 +3,54 @@ const fs = require('fs')
 const makeDir = require('make-dir');
 const server = http.createServer();
 
-const path = require('path')
-const youtubedl = require('youtube-dl')
+var path = require('path');
+var ytdl = require('youtube-dl');
 
-async function playlist(url) {
-
-    'use strict'
-    var downloaded = 0;
-    const video = youtubedl(
+function playlist(url) {
+    // await makeDir(__dirname+ '/uploads')
+    'use strict';
+    var video = ytdl(
         url,
         ['--format=22'],
-        // start will be sent as a range header
+    );
 
-        { start: downloaded }
-    )
-    // console.log("TCL: playlist -> video", video)
     video.on('error', function error(err) {
-        console.log('error 2:', err)
-    })
-
-    video.on('complete', function complete(info) {
-        'use strict';
-        console.log('filename: ' + info._filename + ' already downloaded.');
+        console.log('error 2:', err);
     });
-
 
     let size = 0
-    video.on('info', async function (info) {
+    video.on('info', function (info) {
         size = info.size;
-        await makeDir(__dirname + `/downloads/${info.playlist_uploader}/${info.playlist}/`)
-        let output = path.join(__dirname + `/downloads/${info.playlist_uploader}/${info.playlist}/`, `#0${info.playlist_index} ` + info._filename + '.mp4')
-        if (fs.existsSync(output)) {
-            downloaded = fs.statSync(output).size;
-            console.log("TCL: playlist -> downloaded in if statement", downloaded)
-        }
+        // await makeDir(__dirname + `/downloads/${info.playlist_uploader}/${info.playlist}/`)
+        // var output = path.join(__dirname+ '/' , `${info.playlist_uploader}/${info.playlist}/`,`#0${info.playlist_index} ` +`${info.fulltitle}` + '.mp4');
+        var output = path.join(__dirname+ '/',`#0${info.playlist_index} ` +`${info.fulltitle}` + '.mp4');
+        console.log(`playlist: ${info.playlist} - video:  #0${info.playlist_index} ${info.fulltitle}`)
+        video.pipe(fs.createWriteStream(output));
+    });
 
-        // info.size will be the amount to download, add
-        var total = info.size + downloaded;
-        console.log('size: ' + total);
-        
-        console.log("TCL: playlist -> downloaded", downloaded)
-        if (downloaded > 0) {
-            // size will be the amount already downloaded
-            console.log('resuming from: ' + downloaded);
-
-            // display the remaining bytes to download
-            console.log('remaining bytes: ' + info.size);
-        }
-        console.log('*playlist_index*: ', `#0${info.playlist_index}`, info._filename, info.format_note);
-        video.on('complete', function complete(info) {
-            'use strict';
-            console.log('filename: ' + info._filename + ' already downloaded.');
-        });
-        video.pipe(fs.createWriteStream(output))
-        // Will be called if download was already completed and there is nothing more to download.
-    })
-
-    let pos = 0
+    var pos = 0;
     video.on('data', function data(chunk) {
-        pos += chunk.length
+        pos += chunk.length;
         // `size` should not be 0 here.
         if (size) {
-            let percent = (pos / size * 100).toFixed(2)
-            process.stdout.cursorTo(0)
-            process.stdout.clearLine(1)
-            process.stdout.write(percent + '%')
+            var percent = (pos / size * 100).toFixed(2);
+            process.stdout.cursorTo(0);
+            process.stdout.clearLine(1);
+            process.stdout.write(percent + '%');
         }
-    })
-
-    video.on('end', function () {
-        console.log('finished downloading!');
     });
-    video.on('next', playlist)
+
+    video.on('next', playlist);
+
 }
+
 
 playlist('https://www.youtube.com/playlist?list=PLWKjhJtqVAbnZtkAI3BqcYxKnfWn_C704')
 
 
 const port = process.env.PORT || 3000;
+
+
 
 
 server.listen(port, () => {
