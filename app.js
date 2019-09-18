f267cfe81449020196b6f5d863c950bbe0f5baa5
Abanoub Essam <abanoub@index-soft.com>
2019-09-18 16:46:59 +0200

    in progress sync

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs')
 const makeDir = require('make-dir');
 const server = http.createServer();
-
+const fse = require("fs-extra");
 var path = require('path');
 var ytdl = require('youtube-dl');
 
@@ -22,8 +22,10 @@ function playlist(url) {
     video.on('info', function (info) {
         size = info.size;
         // await makeDir(__dirname + `/downloads/${info.playlist_uploader}/${info.playlist}/`)
-        // var output = path.join(__dirname+ '/' , `${info.playlist_uploader}/${info.playlist}/`,`#0${info.playlist_index} ` +`${info.fulltitle}` + '.mp4');
-        var output = path.join(__dirname+ '/',`#0${info.playlist_index} ` +`${info.fulltitle}` + '.mp4');
+        var dir = __dirname+ `/downloads/${info.playlist_uploader}/${info.playlist}/`;
+        fse.ensureDirSync(dir);
+        var output = path.join(__dirname+ '/downloads' , `${info.playlist_uploader}/${info.playlist}/`,`#0${info.playlist_index} ` +`${info.fulltitle}` + '.mp4');
+        // var output = path.join(__dirname + '/', `#0${info.playlist_index} ` + `${info.fulltitle}` + '.mp4');
         console.log(`playlist: ${info.playlist} - video:  #0${info.playlist_index} ${info.fulltitle}`)
         video.pipe(fs.createWriteStream(output));
     });
